1b5b79c0017a067ed4cddc38d1954383e6025ab6
Rafal Grzenia <rafalgrzenia96@gmail.com>
2024-01-03 12:24:43 +0100

    Add todo list

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 import './styles.css';
+
+// Display/UI
+
+// 1. Populate a board with tiles/mines
+// 2. Left click on tiles
+// a. Reveal tiles
+// 3. Right click on tiles
+// a. Mark tiles
+// 4. Check for win/lose
